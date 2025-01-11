6a818310e71da578395742ce642ef0f83a2f612a
Ekansh Singh <110751935+singh-ekansh@users.noreply.github.com>
2025-01-11 21:38:46 +0530

    Create url.js

---
+++ models/url.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    shortId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    redirectURL: {
+        type: String,
+        required: true
+    },
+    visitHistory:[ {
+        type: Number,
+        default: 0
+    }],
+    },
+    {timestamps: true}
+
+
+);
+
+
+const URL = mongoose.model('URL', urlSchema);
+
+module.exports = URL;
